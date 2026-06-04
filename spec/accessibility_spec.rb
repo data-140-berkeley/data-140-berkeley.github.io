747e2db0c649b94a862a39dfd4e7974a855a9c6d
Silas Santini <70163606+pancakereport@users.noreply.github.com>
2026-06-03 17:40:50 -0700

    don't check graph html

--- spec/accessibility_spec.rb
+++ spec/accessibility_spec.rb
@@ -29,7 +29,7 @@ EXCLUDED_ELEMENTS = [
 # It should be rare to add to this array. One acceptable
 # use is to add redirect pages because they can introduce
 # race conditions and make the a11y tests fail inconsistently.
-SKIPPED_PAGES = [].freeze
+SKIPPED_PAGES = ['/assets/references/mvn.html', '/assets/references/mvn_plane.html'].freeze
 
 # We must call this to ensure the build it up-to-date.
 build_jekyll_site!
