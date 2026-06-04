94935ba11f684bb314cde1ea59f41774b0f684e0
Silas Santini <70163606+pancakereport@users.noreply.github.com>
2026-06-03 17:26:55 -0700

    linting

--- spec/accessibility_spec.rb
+++ spec/accessibility_spec.rb
@@ -10,19 +10,19 @@ require 'spec_helper'
 # See https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#axe-core-tags
 # Tests are segmented in 2.0, 2.1 and 2.2+
 # In most places WCAG 2.1AA is the minimum requirement, but 2.2 is the current WCAG Standard.
-required_a11y_standards = %i[wcag2a wcag2aa wcag21a wcag21aa]
-complete_a11y_standards = %i[wcag22aa best-practice secion508]
+REQUIRED_A11Y_STANDARDS = %i[wcag2a wcag2aa wcag21a wcag21aa].freeze
+COMPLETE_A11Y_STANDARDS = %i[wcag22aa best-practice secion508].freeze
 
 # axe-core rules that are not required to be accessible / do not apply
 # You may temporarily want to add rules here during development.
 # See: https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md
-skipped_rules = []
+SKIPPED_RULES = [].freeze
 # These are elements that are not required to be accessible
 # It should be rare to add to this list. This disables all rules for an element.
 # e.g. <img data-a11y-errors="true" src="..." /> would pass even though it's missing alt text.
-excluded_elements = [
+EXCLUDED_ELEMENTS = [
   '[data-a11y-errors="true"]'
-]
+].freeze
 
 # We must call this to ensure the build it up-to-date.
 build_jekyll_site!
@@ -31,16 +31,16 @@ ALL_PAGES = load_sitemap
 RSpec.shared_examples 'a11y tests' do
   it 'meets WCAG 2.1' do
     expect(page).to be_axe_clean
-      .according_to(*required_a11y_standards)
-      .skipping(*skipped_rules)
-      .excluding(*excluded_elements)
+      .according_to(*REQUIRED_A11Y_STANDARDS)
+      .skipping(*SKIPPED_RULES)
+      .excluding(*EXCLUDED_ELEMENTS)
   end
 
   it 'meets WCAG 2.2' do
     expect(page).to be_axe_clean
-      .according_to(*complete_a11y_standards)
-      .skipping(*skipped_rules)
-      .excluding(*excluded_elements)
+      .according_to(*COMPLETE_A11Y_STANDARDS)
+      .skipping(*SKIPPED_RULES)
+      .excluding(*EXCLUDED_ELEMENTS)
   end
 end
 
